7ef2f4a45117e3e2c5d5b7ddfe7c18eee3f237e8
leshhh-aneee <teddybearlesh@gmail.com>
2023-03-08 17:26:11 +0800

    update html for login

--- source files/login_signup.php
+++ source files/login_signup.php
@@ -16,6 +16,42 @@
     ?>
 
     <body>
-        Login/Signup
+        <section class="about-section text-center">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Login/Sign up</h2>
+                        <p class="text-white-50">
+                            Welcome to SIT Gym 
+                            Wanna save a spot in your favourite class?
+                            No problem - we've got you covered. SIT Gym is filled with features designed
+                            to help you at every step of your health and fitness journey.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br>
+        <form class="container">
+            <div class="column gx-4 gx-lg-5 justify-content-center">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <br>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
     </body>
 </html>
