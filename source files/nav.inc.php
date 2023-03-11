95a5c0b4f37ea6671b027f535b12abe44fad1882
Charista <bryontian@gmail.com>
2023-03-11 12:58:57 +0800

    Moved JS links into footer, Added dropdown for nav

--- source files/nav.inc.php
+++ source files/nav.inc.php
@@ -1,28 +1,30 @@
-<!-- Navigation-->
-
-
-
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="index.php">
-            <img class="brand-logo" src="img/sit_logo.png" alt="sit logo" style="height: 70px;">
-        </a>
-            <button class="navbar-toggler navbar-toggler-right" 
-                type="button" data-bs-toggle="collapse" 
-                data-bs-target="#navbarResponsive" 
-                aria-controls="navbarResponsive" 
-                aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="index.php"><img class="brand-logo" src="img/sit_logo.png" alt="sit logo" style="height: 70px;"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-collapse collapse" id="navbarResponsive">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item"><a class="nav-link" href="membership.php">Membership</a></li>
-                <li class="nav-item"><a class="nav-link" href="classes.php">Classes</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Classes
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Time Table</a>
+                        <a class="dropdown-item" href="#">Explore</a>
+                    </div>
+                </li>
+
                 <li class="nav-item"><a class="nav-link" href="explore.php">Explore</a></li>
                 <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
                 <li class="nav-item"><a class="nav-link" href="location.php">Location</a></li>
                 <li class="nav-item"><a class="nav-link" href="login_signup.php">Login/Signup</a></li>
                 <li class="nav-item"><a class="nav-link" href="about_us.php">About Us</a></li>
+
+                <li class="nav-item">
+
             </ul>
         </div>
     </div>
