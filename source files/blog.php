5416c80a23ae633db4b2ac4103850b0f158ca43e
Charista <bryontian@gmail.com>
2023-03-13 22:04:15 +0800

    Fixed dropdown

--- source files/blog.php
+++ source files/blog.php
@@ -6,10 +6,6 @@
     <title>SIT Active Gym Blog</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script defer src="js/scripts.js"></script>
 </head>
 
 <?php
