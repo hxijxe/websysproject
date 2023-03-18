f9e1f5d41f2be3b87157d81246bf4c61fe89d863
haijie98 <otakujackshu@gmail.com>
2023-03-18 13:27:55 +0800

    edit class name

--- source files/js/scripts.js
+++ source files/js/scripts.js
@@ -8,18 +8,18 @@
 // 
 
 /*Dropdown Menu*/
-$('.lcoation.dropdown').click(function () {
+$('.dropdown').click(function () {
     $(this).attr('tabindex', 1).focus();
     $(this).toggleClass('active');
     $(this).find('.dropdown-menu').slideToggle(300);
 });
-$('.lcoation.dropdown').focusout(function () {
+$('.dropdown').focusout(function () {
     $(this).removeClass('active');
     $(this).find('.dropdown-menu').slideUp(300);
 });
-$('.lcoation.dropdown .dropdown-menu li').click(function () {
-    $(this).parents('.lcoation.dropdown').find('span').text($(this).text());
-    $(this).parents('.lcoation.dropdown').find('input').attr('value', $(this).attr('id'));
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
 });
 /*End Dropdown Menu*/
 
