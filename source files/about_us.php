c4d5f1d4d03c6246085a87fb46b0535574bd3169
hojianrong <jianronggg@gmail.com>
2023-03-22 03:20:00 +0800

    about-us

--- source files/about_us.php
+++ source files/about_us.php
@@ -13,21 +13,90 @@ include "nav.inc.php";
 ?>
 
 <body>
+    <section class="about-section about-background text-center">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="text-black mb-4">WHO WE ARE</h2>
+                    <p class="text-black-50">
+                        Keeping students fit since 2009
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
     <section class="about-section text-center">
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-lg-8">
-                    <h2 class="text-white mb-4">About Us</h2>
-                    <p class="text-white-50">
-                        SIT Gym is a facility meant for the SITizens to achieve their fitness goals amidst of
-                        pursuing their degrees.
+                    <h2 class="text-red mb-4">Our Story</h2>
+                    <p class="text-black-50">
+                    Since our humble beginnings in 1993, we have become one of the largest fitness brands in the world, 
+                    and the largest in Southeast Asia. A trusted and enduring brand, we have an unrivalled network in the region, 
+                    with knowledgeable teams, new technology and world class clubs. Through the years, we’ve seen a lot of changes and 
+                    we’ve come a long way. But our purpose remains clear: we are the fitness leaders who inspire people to go further in life.
+
+                    With more than 230,000 members around the region, we’re always looking for new ways 
+                    to support and inspire them – and not just in the gym.
                     </p>
                 </div>
             </div>
-            <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
         </div>
     </section>
+    <section class="about-section about-background text-center">
+        <div class="container px-5 px-lg-5">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                    <div class="text-center card-header">
+                        <img src="img/sitdover.jpg" class="card-img-top" alt="..." style="background-size: cover; height: 200px;">
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Locate Us</h>
+                        <h5 class="card-text text-dark ">6 Locations across Singapore</h5>
+                    </div>
+                    <div class="text-center card-footer">
+                        <a class="btn btn-secondary mt-3 mb-3" href="location.php">Find Out More</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100" >
+                        <div class="text-center card-header">
+                            <img src="img/zumba.jpg" class="card-img-top" alt="..." style="background-size: cover; height: 200px;">
+                        </div>
+                        <div class="card-body">
+                            <h3 class="card-title">Explore Classes</h3>
+                            <h5 class="card-text text-dark">Check out the classes that we provide</h5>
+                        </div>
+                        <div class="text-center card-footer">
+                            <a class="btn btn-secondary mt-3 mb-3" href="explore.php">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <div class="text-center card-header">
+                            <img src="img/gym-bg.jpg" class="card-img-top" alt="..." style="background-size: cover;height: 200px;">
+                        </div>
+                        <div class="card-body">
+                            <h3 class="card-title">Sign Up with Us</h3>
+                            <h5 class="card-text text-dark ">Kickstart your fitness Journey now</h5>
+                        </div>
+                        <div class="text-center card-footer">
+                            <a class="btn btn-secondary mt-3 mb-3" href="login_signup.php">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 </body>
+
+
+
 <?php
 include "footer.inc.php";
 ?>
