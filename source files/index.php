10e26669b41f0cb42240e34c06fd3dec531cb762
Charista <bryontian@gmail.com>
2023-03-28 22:24:40 +0800

    Nav bar update and index page

--- source files/index.php
+++ source files/index.php
@@ -3,6 +3,7 @@
 
 <head>
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/countdown.css">
     <title>SIT Active Gym</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -13,58 +14,77 @@ include "nav.inc.php";
 ?>
 
 <body>
-    <header class="masthead">
-        <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
-            <div class="d-flex justify-content-center">
-                <div class="text-center">
-                    <h1 class="mx-auto my-0 text-uppercase">SIT Active Gym</h1>
-                    <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by
-                        Start Bootstrap.</h2>
-                    <a class="btn btn-primary" href="#about">Get Started</a>
-                </div>
-            </div>
-        </header>
-
-        <section>
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/cycle.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block carousel-text-bg">
-                            <h5>Weekly Cycling Classes</h5>
-                            <p>Join us for some intense cycling sessions</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/swim.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block carousel-text-bg">
-                            <h5>Learn how to swim</h5>
-                            <p>Let our professional instructors teach you how to swim</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/zumba.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block carousel-text-bg">
-                            <h5>Try out our Zumba Classes</h5>
-                            <p>Our energetic instructors will make you work up a sweat</p>
-                        </div>
+    <section>
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item">
+                    <img src="img/cycle.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block carousel-text-bg">
+                        <h5>Weekly Cycling Classes</h5>
+                        <p>Join us for some intense cycling sessions</p>
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="carousel-item active">
+                    <img src="img/sitnew.jpg" class="d-block w-100" alt="...">
+                    <div class="container">
+                        <div id="clockdiv">
+                            <div>
+                                <span class="days"></span>
+                                <div class="smalltext">Days</div>
+                            </div>
+                            <div>
+                                <span class="hours"></span>
+                                <div class="smalltext">Hours</div>
+                            </div>
+                            <div>
+                                <span class="minutes"></span>
+                                <div class="smalltext">Minutes</div>
+                            </div>
+                            <div>
+                                <span class="seconds"></span>
+                                <div class="smalltext">Seconds</div>
+                            </div>
+                        </div>
+                        <div class="carousel-caption2 d-none d-md-block carousel-text-bg">
+                            <div class="card__content padding-large">
+                                <h2>COMING SOON!</h2>
+                                <div class="rowmap">
+                                    <a href="https://www.singaporetech.edu.sg/" class="RX4Lf26" target="_blank" rel="noreferrer">
+                                        <p class="kD9RoPu">View on map</p>
+                                    </a>
+                                </div>
+                                <p>SIT @ PUNGGOL</p>
+                                <p>Reimagining Education as an Applied Learning University</p>
+                                <a href="location_SITNYP.php" class="RX4Lf26" target="_blank" rel="noreferrer">
+                                    <div class="card__button">Find Out More</div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/zumba.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block carousel-text-bg">
+                        <h5>Join Our Blog Post</h5>
+                        <a class="btn btn-secondary mt-3 mb-3" href="blog.php">Blog Page</a>
+                    </div>
+                </div>
             </div>
-        </section>
+            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
         <section class="projects-section bg-body" id="projects">
             <div class="container px-5 px-lg-5">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
@@ -133,6 +153,7 @@ include "nav.inc.php";
             integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
             crossorigin="anonymous">
         </script>
-
+        <script defer src="js/scripts.js"></script>
+        <script defer src="js/countdown.js"></script>
     </body>
 </html>
--- source files/nav.inc.php
+++ source files/nav.inc.php
@@ -1,9 +1,8 @@
-<!--Font Awesome Javascript-->
-<script src="https://kit.fontawesome.com/3d68f572d4.js" crossorigin="anonymous"></script>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-black fixed-top" style="background-color:black;">
+
+<nav class="navbar navbar-expand-xl navbar-dark bg-black fixed-top">
     <div class="container px-4 px-lg-5">
-    <a class="navbar-brand px-3" href="index.php"><img class="brand-logo" src="img/sit_logo.png" alt="sit logo" style="height: 80px;"></a>
+        <a class="navbar-brand flexstart" href="index.php"><img class="brand-logo" src="img/sit_logo.png" alt="sit logo" style="height: 80px;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -12,8 +11,8 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item"><a class="nav-link" href="membership.php">Membership</a></li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                <li class="nav-item dropdown ">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
                         Classes
                     </a>
@@ -25,26 +24,12 @@
 
                 <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
                 <li class="nav-item"><a class="nav-link" href="location.php">Location</a></li>
-<!--                <li class="nav-item"><a class="nav-link" href="login_signup.php">Login/Signup</a></li>-->
                 <li class="nav-item"><a class="nav-link" href="about_us.php">About Us</a></li>
-
-                <!--login icon-->
-                <li class="nav-item"><a class="nav-link" href="login_signup.php"><i class="fa-solid fa-user fa-lg" style="color: #000000;"></i> Sign In</a></li>
-
-                <!--Checks if user is logged in, if so user has the option to logout-->
-                <?php
-                if (isset($_SESSION['user_id'])):
-                ?>
-
-                <!--profile icon-->
-                <li class="nav-item"><a class="nav-link link-primary" href="profile.php"><i class="fa-solid fa-id-card fa-lg" style="color: #da3832;"></i> Profile</a></li>
-                <!--logout icon-->
-                <li class="nav-item"><a class="nav-link link-primary" href="logout.php"><i class="fa-solid fa-right-from-bracket fa-lg" style="color: #000000;"></i> Logout</a></li>
-                <?php
-                endif;
-                ?>
-
             </ul>
+            <div>
+                <a href="profile.php" class="nav-btn">Profile</a>
+                <a href="login_signup.php" class="nav-btn">Login/Signup</a>
+            </div>
         </div>
     </div>
 </nav>
