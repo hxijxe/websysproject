0e6717f623a0e0c289617e490ca35a076425a227
leshhh-aneee <teddybearlesh@gmail.com>
2023-03-30 12:11:04 +0800

    change login/register buttons

--- source files/login_signup.php
+++ source files/login_signup.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+    <a href="../login_signup.php">Login & Register</a>
 </head>
 <?php
 include "nav.inc.php";
@@ -69,11 +70,6 @@ include "nav.inc.php";
                         <span>or use your account</span>
                         <div class="social-container">
                             <a href="#" class="social">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="-120 0 588 512">
-                                    <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
-                                </svg>
-                            </a>
-                            <a href="#" class="social">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 0 588 512">
                                     <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
                                 </svg>
@@ -109,11 +105,6 @@ include "nav.inc.php";
                         <!-- Login using social media -->
                         <span>or use your account</span>
                         <div class="social-container">
-                            <a href="#" class="social">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="-120 0 588 512">
-                                    <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
-                                </svg>
-                            </a>
                             <a href="#" class="social">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 0 588 512">
                                     <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
