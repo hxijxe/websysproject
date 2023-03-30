a80e854a07f871021de881ce729c6c86e5007c23
Faith <faithlimzx@gmail.com>
2023-03-30 15:59:46 +0800

    admin-feedback

---
+++ source files/adminbooking(del).php
@@ -0,0 +1,218 @@
+<?php
+// Start the session
+//include "includes/protect.php";
+?>
+<!DOCTYPE html>
+<html lang="en-GB">
+
+<head>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <!-- <script defer src="js/admin.js"></script> -->
+    <title>Admin </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+
+<?php
+include "nav.inc.php";
+?>
+
+<body id=test>
+
+
+    <main>
+        <!-- ======= Contact Section ======= -->
+
+        <?php
+        if (isset($_POST["accept"])) {
+
+            // Create database connection.
+            $config = parse_ini_file('../../private/db-config.ini');
+            $conn = new mysqli(
+                $config['servername'],
+                $config['username'],
+                $config['password'],
+                $config['dbname']
+            );
+            // Check connection
+            if ($conn->connect_error) {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                echo '<script>alert(' . $errorMsg . ')</script>';
+            } else {
+                // // Prepare the statement:
+                // try {
+                //     $sql = "UPDATE bookings SET bkStatus=\"accepted\" WHERE booking_id=" . $_POST['booking_id'] . ";";
+                //     $result = $conn->query($sql);
+                //     echo '<script>alert("Accepted booking by ' . $_POST['name'] . ' on ' . $_POST['date'] . ' for ' . $_POST['pax'] . ' pax")</script>';
+                //     sendMail("accepted", $_POST['email'], $_POST['name'], $_POST['date'], $_POST['pax']);
+                // } catch (Exception $ex) {
+                //     echo '<script>alert(' . $ex . ')</script>';
+                // }
+            }
+            $conn->close();
+        } else if (isset($_POST["reject"]))
+            // Create database connection.
+            $config = parse_ini_file('../../private/db-config.ini');
+        $conn = new mysqli(
+            $config['servername'],
+            $config['username'],
+            $config['password'],
+            $config['dbname']
+        );
+        // Check connection
+        if ($conn->connect_error) {
+            $errorMsg = "Connection failed: " . $conn->connect_error;
+            echo '<script>alert(' . $errorMsg . ')</script>';
+            // } else {
+            //     // Prepare the statement:
+            //     try {
+            //         $sql = "UPDATE bookings SET bkStatus=\"rejected\" WHERE bookingID=" . $_POST['bookingID'] . ";";
+            //         $result = $conn->query($sql);
+            //         echo '<script>alert("Rejected booking by ' . $_POST['name'] . ' on ' . $_POST['date'] . ' for ' . $_POST['pax'] . ' pax")</script>';
+            //         sendMail("declined", $_POST['email'], $_POST['name'], $_POST['date'], $_POST['pax']);
+            //     } catch (Exception $ex) {
+            //         echo '<script>alert(' . $ex . ')</script>';
+            //     }
+            // }
+            $conn->close();
+        }
+
+
+        function retrieveDB()
+        {
+            // Create database connection.
+            $config = parse_ini_file('../../private/db-config.ini');
+            $conn = new mysqli(
+                $config['servername'],
+                $config['username'],
+                $config['password'],
+                $config['dbname']
+            );
+            // Check connection
+            if ($conn->connect_error) {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                $success = false;
+                echo '<script>alert(' . $errorMsg . ')</script>';
+            } else {
+
+                // Prepare the statement:
+                try {
+
+                    $sql = "SELECT * FROM bookings WHERE bkStatus='pending'";
+                    $result = $conn->query($sql);
+
+                    $blogs = array();
+
+                    while ($row = $result->fetch_assoc()) {
+                        $blogs[] = $row['Blog_Contents'];
+                    };
+
+                    $max = sizeof($blogs);
+                    //echo '<script>alert(' . $max . ')</script>';
+                    $count = 0;
+
+                    $result = $conn->query($sql);
+                    if ($max != 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            //echo '<script>alert(' . $max . ')</script>';
+                            // echo '<script>alert(' . $count . ')</script>';
+                            //echo "Name: " . $row["bkName"] . "<br>";
+
+                            if (($count + 2) % 2 == 0) {
+                                echo "<div style=\"background:bisque; padding: 10px 10px 20px;\">";
+                            } else {
+                                echo "<div style=\"background:white; padding: 10px 10px 20px;\">";
+                            }
+                            echo "<h3><b>Booking Type:</b> " . $row["bookingType"] . "</h3>" .
+                                "<b><p>Date:</b> " . $row["reserveDate"] . "</p>";
+                            if ($row["bookingType"] == "normal") {
+                                echo "<b><p>Time:</b> " . $row["timeslot"] . "</p>";
+                            } else {
+                                echo "<p>Event Type:</b> " . $row["eventType"] . "</p>";
+                            }
+                            echo "<b><p>Name:</b> " . $row["bkName"] . "</p>";
+                            echo "<b><p>Pax:</b> " . $row["pax"] . "</p>" .
+                                "<b><p>EMail:</b> " . $row["bkEmail"] . "</p>";
+                            "<b><p>Phone:</b> " . $row["phoneNo"] . "</p>";
+                            //need css to look like btn
+                            //"<p id=\"bbtn\" name=\"".$row["bookingID"]."\"class=\"acceptBtn\">Accept</p>" .
+                            //"<p id=\"bbtn\" class=\"rejectBtn\">Decline</p></div>";
+
+                            echo "<div class=\"formd\"><form class=\"formd\" action=\"\" method=\"post\" name=\"accept\">" .
+                                "<input type=\"hidden\" name=\"bookingID\" id=\"bookingID\" value= " . $row["bookingID"] . ">" .
+                                "<input type=\"hidden\" name=\"name\" id=\"name\" value= " . $row["bkName"] . ">" .
+                                "<input type=\"hidden\" name=\"date\" id=\"date\" value= " . $row["reserveDate"] . ">" .
+                                "<input type=\"hidden\" name=\"pax\" id=\"pax\" value= " . $row["pax"] . ">" .
+                                "<input type=\"hidden\" name=\"email\" id=\"email\" value= " . $row["bkEmail"] . ">" .
+                                "<button class=\"bbtn\" type=\"submit\" name=\"accept\">Accept</button></form>";
+
+                            echo "<form class=\"formd\" action=\"\" method=\"post\" name=\"reject\">" .
+                                "<input type=\"hidden\" name=\"bookingID\" id=\"bookingID\" value= " . $row["bookingID"] . ">" .
+                                "<input type=\"hidden\" name=\"name\" id=\"name\" value= " . $row["bkName"] . ">" .
+                                "<input type=\"hidden\" name=\"date\" id=\"date\" value= " . $row["reserveDate"] . ">" .
+                                "<input type=\"hidden\" name=\"pax\" id=\"pax\" value= " . $row["pax"] . ">" .
+                                "<input type=\"hidden\" name=\"email\" id=\"email\" value= " . $row["bkEmail"] . ">" .
+                                "<button class=\"bbtn\" type=\"submit\" name=\"reject\">Reject</button></form></div></div>";
+
+
+                            $count += 1;
+                        }
+                    } else {
+                        echo "No incoming bookings";
+                    }
+                } catch (Exception $ex) {
+                    echo '<script>alert(' . $ex . ')</script>';
+                }
+            }
+            $conn->close();
+        }
+        ?>
+
+        <div id=test>
+            <h2>Dashboard</h2>
+            <input id="tab1" type="radio" name="tabs" checked>
+            <label for="tab1">Manage Classes</label>
+            <input id="tab2" type="radio" name="tabs">
+            <label for="tab2">Manage Blogposts</label>
+            <input id="tab3" type="radio" name="tabs">
+            <label for="tab3">Manage Feedback</label>
+
+            <section id="content1">
+                <?php
+                retrieveDB();
+                ?>
+
+
+            </section>
+
+            <section id="content2">
+                <h3>Classes</h3>
+                
+            </section>
+            <section id="content3">
+                <h3>Heading Text</h3>
+                <p>Fusce pulvinar porttitor dui, eget ultrices nulla tincidunt vel. Suspendisse faucibus lacinia tellus, et viverra ligula. Suspendisse eget ipsum auctor, congue metus vel, dictum erat. Aenean tristique euismod molestie. Nulla rutrum accumsan nisl, ac semper sapien tincidunt et. Praesent tortor risus, commodo et sagittis nec, aliquam quis augue. Aenean non elit elementum, tempor metus at, aliquam felis.</p>
+            </section>
+
+        </div>
+    </main>
+
+    <div>
+        <?php
+        include "footer.inc.php";
+        ?>
+        <script defer src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+        </script>
+        <!--Bootstrap JS-->
+        <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous">
+        </script>
+        <script defer src="js/scripts.js"></script>
+    </div>
+</body>
+
+
+</html>
--- source files/adminbooking.php
+++ source files/adminbooking.php
@@ -16,164 +16,17 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 
-
 <?php
 include "nav.inc.php";
 ?>
 
 <body id=test>
 
-
     <main>
         <!-- ======= Contact Section ======= -->
 
-        <?php
-        if (isset($_POST["accept"])) {
-
-            // Create database connection.
-            $config = parse_ini_file('../../private/db-config.ini');
-            $conn = new mysqli(
-                $config['servername'],
-                $config['username'],
-                $config['password'],
-                $config['dbname']
-            );
-            // Check connection
-            if ($conn->connect_error) {
-                $errorMsg = "Connection failed: " . $conn->connect_error;
-                echo '<script>alert(' . $errorMsg . ')</script>';
-            } else {
-                // // Prepare the statement:
-                // try {
-                //     $sql = "UPDATE bookings SET bkStatus=\"accepted\" WHERE booking_id=" . $_POST['booking_id'] . ";";
-                //     $result = $conn->query($sql);
-                //     echo '<script>alert("Accepted booking by ' . $_POST['name'] . ' on ' . $_POST['date'] . ' for ' . $_POST['pax'] . ' pax")</script>';
-                //     sendMail("accepted", $_POST['email'], $_POST['name'], $_POST['date'], $_POST['pax']);
-                // } catch (Exception $ex) {
-                //     echo '<script>alert(' . $ex . ')</script>';
-                // }
-            }
-            $conn->close();
-        } else if (isset($_POST["reject"]))
-            // Create database connection.
-            $config = parse_ini_file('../../private/db-config.ini');
-        $conn = new mysqli(
-            $config['servername'],
-            $config['username'],
-            $config['password'],
-            $config['dbname']
-        );
-        // Check connection
-        if ($conn->connect_error) {
-            $errorMsg = "Connection failed: " . $conn->connect_error;
-            echo '<script>alert(' . $errorMsg . ')</script>';
-            // } else {
-            //     // Prepare the statement:
-            //     try {
-            //         $sql = "UPDATE bookings SET bkStatus=\"rejected\" WHERE bookingID=" . $_POST['bookingID'] . ";";
-            //         $result = $conn->query($sql);
-            //         echo '<script>alert("Rejected booking by ' . $_POST['name'] . ' on ' . $_POST['date'] . ' for ' . $_POST['pax'] . ' pax")</script>';
-            //         sendMail("declined", $_POST['email'], $_POST['name'], $_POST['date'], $_POST['pax']);
-            //     } catch (Exception $ex) {
-            //         echo '<script>alert(' . $ex . ')</script>';
-            //     }
-            // }
-            $conn->close();
-        }
-
-
-        function retrieveDB()
-        {
-            // Create database connection.
-            $config = parse_ini_file('../../private/db-config.ini');
-            $conn = new mysqli(
-                $config['servername'],
-                $config['username'],
-                $config['password'],
-                $config['dbname']
-            );
-            // Check connection
-            if ($conn->connect_error) {
-                $errorMsg = "Connection failed: " . $conn->connect_error;
-                $success = false;
-                echo '<script>alert(' . $errorMsg . ')</script>';
-            } else {
-
-                // Prepare the statement:
-                try {
-
-                    $sql = "SELECT * FROM bookings WHERE bkStatus='pending'";
-                    $result = $conn->query($sql);
-
-                    $blogs = array();
-
-                    while ($row = $result->fetch_assoc()) {
-                        $blogs[] = $row['Blog_Contents'];
-                    };
-
-                    $max = sizeof($blogs);
-                    //echo '<script>alert(' . $max . ')</script>';
-                    $count = 0;
-
-                    $result = $conn->query($sql);
-                    if ($max != 0) {
-                        while ($row = $result->fetch_assoc()) {
-                            //echo '<script>alert(' . $max . ')</script>';
-                            // echo '<script>alert(' . $count . ')</script>';
-                            //echo "Name: " . $row["bkName"] . "<br>";
-
-                            if (($count + 2) % 2 == 0) {
-                                echo "<div style=\"background:bisque; padding: 10px 10px 20px;\">";
-                            } else {
-                                echo "<div style=\"background:white; padding: 10px 10px 20px;\">";
-                            }
-                            echo "<h3><b>Booking Type:</b> " . $row["bookingType"] . "</h3>" .
-                                "<b><p>Date:</b> " . $row["reserveDate"] . "</p>";
-                            if ($row["bookingType"] == "normal") {
-                                echo "<b><p>Time:</b> " . $row["timeslot"] . "</p>";
-                            } else {
-                                echo "<p>Event Type:</b> " . $row["eventType"] . "</p>";
-                            }
-                            echo "<b><p>Name:</b> " . $row["bkName"] . "</p>";
-                            echo "<b><p>Pax:</b> " . $row["pax"] . "</p>" .
-                                "<b><p>EMail:</b> " . $row["bkEmail"] . "</p>";
-                            "<b><p>Phone:</b> " . $row["phoneNo"] . "</p>";
-                            //need css to look like btn
-                            //"<p id=\"bbtn\" name=\"".$row["bookingID"]."\"class=\"acceptBtn\">Accept</p>" .
-                            //"<p id=\"bbtn\" class=\"rejectBtn\">Decline</p></div>";
-
-                            echo "<div class=\"formd\"><form class=\"formd\" action=\"\" method=\"post\" name=\"accept\">" .
-                                "<input type=\"hidden\" name=\"bookingID\" id=\"bookingID\" value= " . $row["bookingID"] . ">" .
-                                "<input type=\"hidden\" name=\"name\" id=\"name\" value= " . $row["bkName"] . ">" .
-                                "<input type=\"hidden\" name=\"date\" id=\"date\" value= " . $row["reserveDate"] . ">" .
-                                "<input type=\"hidden\" name=\"pax\" id=\"pax\" value= " . $row["pax"] . ">" .
-                                "<input type=\"hidden\" name=\"email\" id=\"email\" value= " . $row["bkEmail"] . ">" .
-                                "<button class=\"bbtn\" type=\"submit\" name=\"accept\">Accept</button></form>";
-
-                            echo "<form class=\"formd\" action=\"\" method=\"post\" name=\"reject\">" .
-                                "<input type=\"hidden\" name=\"bookingID\" id=\"bookingID\" value= " . $row["bookingID"] . ">" .
-                                "<input type=\"hidden\" name=\"name\" id=\"name\" value= " . $row["bkName"] . ">" .
-                                "<input type=\"hidden\" name=\"date\" id=\"date\" value= " . $row["reserveDate"] . ">" .
-                                "<input type=\"hidden\" name=\"pax\" id=\"pax\" value= " . $row["pax"] . ">" .
-                                "<input type=\"hidden\" name=\"email\" id=\"email\" value= " . $row["bkEmail"] . ">" .
-                                "<button class=\"bbtn\" type=\"submit\" name=\"reject\">Reject</button></form></div></div>";
-
-
-                            $count += 1;
-                        }
-                    } else {
-                        echo "No incoming bookings";
-                    }
-                } catch (Exception $ex) {
-                    echo '<script>alert(' . $ex . ')</script>';
-                }
-            }
-            $conn->close();
-        }
-        ?>
-
         <div id=test>
-            <h2>dashboard</h2>
+            <h2>Dashboard</h2>
             <input id="tab1" type="radio" name="tabs" checked>
             <label for="tab1">Manage Classes</label>
             <input id="tab2" type="radio" name="tabs">
@@ -182,36 +35,112 @@ include "nav.inc.php";
             <label for="tab3">Manage Feedback</label>
 
             <section id="content1">
-                <?php
-                retrieveDB();
-                ?>
 
+                <h2 class="py-3 my-3">Add Classes</h2>
+                <div class="classform">
+                    <form action="processBooking.php" method="POST">
+                        <label for="name">Location</label>
+                        <select id="location" name="location">
+                            <option value="Dover">Dover</option>
+                            <option value="TP">TP</option>
+                            <option value="NYP">NYP</option>
+                            <option value="SP">SP</option>
+                            <option value="RP">RP</option>
+                            <option value="NP">NP</option>
+                        </select>
+
+                        <label for="className">Choose Class</label>
+                        <select id="className" name="className">
+                            <option value="yoga">Yoga</option>
+                            <option value="zumba">Zumba</option>
+                            <option value="gym">GYM</option>
+                            <option value="cycleBurn">Cycle Burn</option>
+                            <option value="swimming">Swimming</option>
+                        </select>
+
+                        <label for="timeslot">Choose your Time Slot</label>
+                        <select id="timeslot" name="timeslot">
+                            <option value="7am-9am">7am-9am</option>
+                            <option value="10am-12pm">10am-12pm</option>
+                            <option value="1pm-3pm">1pm-3pm</option>
+                            <option value="4pm-5pm">4pm-5pm</option>
+                            <option value="6pm-8pm">6pm-8pm</option>
+                        </select>
+
+                        <label for="instructor">Choose your instructor</label>
+                        <select id="instructor" name="instructor">
+                            <option value="leshane">Ms Leshane</option>
+                            <option value="bryon">Mr Bryon</option>
+                            <option value="faith">Ms Faith</option>
+                            <option value="ho">Mr Ho</option>
+                            <option value="jack">Mr Jack</option>
+                        </select>
+
+                        <!-- <input type="submit" value="Submit"> -->
+                        <a class="btn btn-secondary mt-3 mb-3" input type="submit" value="Submit">Make Changes</a>
+                    </form>
+                </div>
 
             </section>
-            <section id="content2">
-                <h3>Something</h3>
-                <p>Aliquam condimentum hendrerit nisi, nec vestibulum mi feugiat ut. Donec lobortis nisi neque, in egestas eros venenatis eu. Vestibulum nulla nisi, venenatis at pretium id, fermentum quis risus. Quisque porta suscipit neque eu placerat. Etiam scelerisque, quam in sodales iaculis, velit velit mattis nunc, quis dapibus massa elit nec enim. Vivamus quis libero aliquam, volutpat nisl sed, consectetur ante. Donec aliquam semper felis, in placerat leo blandit in. Integer interdum elit quis felis tempor venenatis. In faucibus ac mauris id commodo. Proin in sapien tincidunt, luctus mi id, bibendum dui. Nunc tincidunt libero ut purus vehicula, sit amet tincidunt mi sollicitudin. Donec varius erat magna, sed convallis purus adipiscing ut. Duis sagittis ut leo ut auctor. Ut convallis nisl nec purus sollicitudin, nec iaculis felis rutrum. </p>
+
+
+            <section id="content2 ">
+                <div class="table-wrapper">
+                    <h3>Update Blogposts</h3>
+
+                    <table class="fl-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Feedback</th>
+                                <th>Rating</th>
+
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </section>
+
             <section id="content3">
-                <h3>Heading Text</h3>
-                <p>Fusce pulvinar porttitor dui, eget ultrices nulla tincidunt vel. Suspendisse faucibus lacinia tellus, et viverra ligula. Suspendisse eget ipsum auctor, congue metus vel, dictum erat. Aenean tristique euismod molestie. Nulla rutrum accumsan nisl, ac semper sapien tincidunt et. Praesent tortor risus, commodo et sagittis nec, aliquam quis augue. Aenean non elit elementum, tempor metus at, aliquam felis.</p>
+                <div class="table-wrapper">
+                    <h3>View Feedback:</h3>
+                    <table class="fl-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Feedback</th>
+                                <th>Rating</th>
+
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
             </section>
+
+
 
         </div>
     </main>
 
-    <div>
-        <?php
-        include "footer.inc.php";
-        ?>
+
+
+
+
+</body>
+<!-- <div>
+        //<?php
+            //include "footer.inc.php";
+            //
+            ?>
         <script defer src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
         </script>
         <!--Bootstrap JS-->
-        <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous">
+<!-- <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous">
         </script>
         <script defer src="js/scripts.js"></script>
-    </div>
-</body>
-
+    </div> -->
 
 </html>
