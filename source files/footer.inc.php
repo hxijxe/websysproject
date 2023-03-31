c18624a47256854904d8bc92f27960e8e262e339
Charista <bryontian@gmail.com>
2023-03-31 16:14:38 +0800

    Adding Footing Citations

--- source files/footer.inc.php
+++ source files/footer.inc.php
@@ -1,6 +1,6 @@
 <footer class="footer bg-black small text-center text-white">
     <div class="container">
-        <div class="d-flex flex-wrap justify-content-between align-items-center py-5 my-3 border-top " >
+        <div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-3 border-top " >
             <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
 
             <a href="index.php" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
@@ -16,5 +16,8 @@
                 <li class="nav-item"><a class="nav-link px-2 text-muted" href="about_us.php">About Us</a></li>
             </ul>
         </div>
+        <p class="foot_cite">Campus locations. Campus Locations | Singapore Institute of Technology. (n.d.). Retrieved from https://www.singaporetech.edu.sg/campus-locations </p>
+        <p class="foot_cite">Pexels. (n.d.). Retrieved from https://www.pexels.com/ </p>
+        <p class="foot_cite">Simms, D. (2022, July 9). This gigantic new luxury gym in LA is part fitness center, part stylish member's club. Robb Report. Retrieved from https://robbreport.com/lifestyle/health-wellness/inside-heimat-worlds-first-luxury-gym-concept-1234697061/ </p>
     </div>
 </footer>
