0bf269d4d02f79f6d689337eef6427555f02e399
haijie98 <otakujackshu@gmail.com>
2023-03-31 17:01:39 +0800

    idk

--- source files/classes.php
+++ source files/classes.php
@@ -56,7 +56,7 @@ include "nav.inc.php";
 
             <h2 class="py-3 my-3">All Available Classes</h2>
             <a class="btn btn-secondary mt-3 mb-3" href="booking.php">Book Now!</a>
-            <div class="mb-5 table-responsive" style="overflow-x:auto;">
+            <div class="mb-5 table-responsive" style="overflow-x:auto;" tabindex="0">
                 <?php
                 $timeslot = $duration = $class = $instructor = $location = $errorMsg = "";
                 $success = true;
